d2e2b59eaed2100e67dbfa8e412199a23a79ed22
enthusiast17 <ulan17nurym@gmail.com>
2020-01-24 17:11:50 +0600

    updated f3s and readme

--- README.md
+++ README.md
@@ -10,8 +10,6 @@ go get github.com/enthusiast17/f3s
 
 ### Usage
 
-Attention! You must use `flags.Parse(true/false)` after setting flags, otherwise flags won't be parsed.
-
 ```Go
 package main
 
@@ -22,30 +20,16 @@ import (
 )
 
 func main() {
-        // Create new flags
-	flags := f3s.SetNewFlags()
+    // Create new flags
+	flags := f3s.SetNewFlags(os.Args[1:])
 
-	// 1st param is flag and 2nd param is description
-	flags.SetFlag("output", "This flag will write ascii output to file")
+	// SetFlag returns a flag's content, 1st param is flag and 2nd param is description
+	output := flags.SetFlag("output", "This flag will write ascii output to file")
 
-	// 1st param is flag and 2nd param is description
-	flags.SetFlag("reverse", "This flag will convert ascii file to string")
+	// SetFlag returns a flag's content, 1st param is flag and 2nd param is description
+	reverse := flags.SetFlag("reverse", "This flag will convert ascii file to string")
 
-	// Parse function returns parsed flags
-	// Bool param is displayHelper if true displays --help/-h command when there is no argument, else displays nothing
-	flags.Parse(false)
-
-        // HasContent returns a bool, if the given tag has a content returns 'true', else returns 'false'.
-	if f3s.HasContent(flags, "output") {
-		output := f3s.Content(flags, "output") // fs.Content returns a content from parsed flags
-		fmt.Println(output)
-	}
-
-	// HasContent returns a bool, if the given tag has a content returns 'true', else returns 'false'.
-	if f3s.HasContent(flags, "reverse") {
-		reverse := f3s.Content(flags, "reverse") // fs.Content returns a content from parsed flags
-		fmt.Println(reverse)
-	}
+	fmt.Println(output, reverse)
 }
 
 ```
--- f3s.go
+++ f3s.go
@@ -1,85 +1,64 @@
 package f3s
 
-import (
-	"fmt"
-	"os"
-)
-
-// Flag a struct with tag, content, dec params.
+// Flag a struct with tag, content, des params.
 type Flag struct {
-	tag     string
-	content string
-	dec     string
+	tag     string // Flag's tag
+	content string // Flag's content
+	des     string // Flag's description
 }
 
 // Flags a struct with Flag's array param.
 type Flags struct {
-	flags []*Flag
+	flags map[string]*Flag
 }
 
-// SetNewFlags returns a new Flags's object.
-func SetNewFlags() *Flags {
-	return &Flags{}
-}
-
-// Parse returns a Flags object with parsed flags or displays helper if there is no arguments.
-func (flags *Flags) Parse(displayHelper bool) *Flags {
-	args := os.Args[1:]
-	if displayHelper {
-		if len(args) == 0 || (len(args) == 1 && (args[0] == "-h" || args[0] == "--help")) {
-			for _, flag := range flags.flags {
-				fmt.Printf("--%s\n\t%s\n", flag.tag, flag.dec)
-			}
-			os.Exit(0)
-		}
-	}
-	for _, flag := range flags.flags {
-		for _, arg := range args {
-			f := ("--" + flag.tag + "=")
-			if len(arg) > len(f) && arg[:len(f)] == f {
-				flag.content = arg[len(f):]
-				break
+// SetNewFlags returns a new Flags's object with parsed flags.
+func SetNewFlags(args []string) *Flags {
+	flags := make(map[string]*Flag)
+	for _, arg := range args {
+		hyphen := -1
+	r:
+		for j, r := range []rune(arg) {
+			switch r {
+			case '-':
+				if j <= 1 {
+					hyphen = j
+				} else {
+					break r
+				}
+			case '=':
+				if j+1 < len(arg) && hyphen > -1 && hyphen+1 < len(arg) {
+					flag := &Flag{tag: arg[hyphen+1 : j], content: arg[j+1:]}
+					flags[flag.tag] = flag
+				}
+				break r
 			}
 		}
 	}
-	return flags
+	return &Flags{flags: flags}
 }
 
-/* I will consider about it later ;)
-func SetFlag(tag string, dec string) *Flags {
-	return parse(tag, dec)
-}
-*/
-
-// SetFlag returns a Flags object with tag and dec.
-func (flags *Flags) SetFlag(tag string, dec string) *Flags {
-	var newFlags []*Flag
-	if flags != nil {
-		newFlags = append(newFlags, flags.flags...)
+// SetFlag returns a Flag's content value.
+func (flags *Flags) SetFlag(tag string, des string) string {
+	flag := flags.flags[tag]
+	var content string
+	if flag != nil {
+		flag.des = des
+		content = flag.content
+	} else {
+		flags.flags[tag] = &Flag{tag: tag, des: des}
 	}
-	newFlag := &Flag{tag: tag, dec: dec}
-	newFlags = append(newFlags, newFlag)
-	flags.flags = newFlags
-	return flags
-}
-
-// GetMap returns a map with flags and contents.
-func GetMap(flags *Flags) map[string]string {
-	var m map[string]string
-	for _, flag := range flags.flags {
-		if flag != nil {
-			m[flag.tag] = flag.tag
-		}
-	}
-	return m
+	return content
 }
 
 // GetContents returns an array with contents.
 func GetContents(flags *Flags) []string {
 	var contents []string
-	for _, flag := range flags.flags {
-		if flag != nil {
-			contents = append(contents, flag.content)
+	if flags != nil {
+		for _, flag := range flags.flags {
+			if flag != nil {
+				contents = append(contents, flag.content)
+			}
 		}
 	}
 	return contents
@@ -88,34 +67,23 @@ func GetContents(flags *Flags) []string {
 // GetTags returns an array with tags.
 func GetTags(flags *Flags) []string {
 	var tags []string
-	for _, flag := range flags.flags {
-		if flag != nil {
-			tags = append(tags, flag.tag)
+	if flags != nil {
+		for tag := range flags.flags {
+			tags = append(tags, tag)
 		}
 	}
 	return tags
 }
 
-// Content returns a content from given Flags object.
-func Content(flags *Flags, tag string) string {
-	var content string
-	for _, flag := range flags.flags {
-		if flag != nil && flag.tag == tag {
-			content = flag.content
-			break
-		}
-	}
-	return content
-}
-
-// HasContent returns a bool, if the given tag has a content returns 'true', else returns 'false'.
-func HasContent(flags *Flags, tag string) bool {
-	for _, flag := range flags.flags {
-		if flag != nil && flag.tag == tag {
-			if flag.content != "" || len(flag.content) != 0 {
-				return true
+// GetDescriptions returns an array with descriptions.
+func GetDescriptions(flags *Flags) []string {
+	var dess []string
+	if flags != nil {
+		for _, flag := range flags.flags {
+			if flag != nil {
+				dess = append(dess, flag.des)
 			}
 		}
 	}
-	return false
+	return dess
 }
